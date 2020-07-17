cc4a5fa7057f987738f449fee2516ce468ea5af1
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2020-07-17 14:58:01 +0100

    Change onClick event handler
    It replaces what comes after the right arrow (alert('click')with this.setState

--- src/index.js
+++ src/index.js
@@ -14,7 +14,8 @@ class Square extends React.Component {
   render() {
     return (
       // Use arrow function syntax
-      <button className="square" onClick={() => alert('click')}>
+      <button className="square" onClick={() => this.setState({value: 'X'})}
+      >
         {this.state.value}
       </button>
     );
